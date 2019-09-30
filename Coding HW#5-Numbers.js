775680cbf2190f0b1313a329b0b9c558634e9cfc
BrianJ-Code <55200248+BrianJ-Code@users.noreply.github.com>
2019-09-29 23:08:44 -0400

    Create Coding HW#5-Numbers.js

---
+++ Coding HW#5-Numbers.js
@@ -0,0 +1,27 @@
+//Math Operations
+const num1 = 100;
+const numb2 = 50;
+let val;
+
+val = num1 + num2; //Addition
+val = num1 * num2; //Multiple
+val = num1 - num2; //Subtraction
+val = num1 / num2; //Division
+val = num1 % num2; //Remainder
+
+//Math Objects
+val = Math.PI; //Math is an object which has properties called attributes
+val = Math.E; //Euler's Number
+val = Math.round(2.8);//Rounds to three
+val = Math.ceil(2.4);//Still rounds to three
+val = Math.floor(2.8);//Rounds to two
+val = Math.sqrt(64);//8
+val = Math.abs(-3);//3
+val = Math.pow(8, 2);
+val = Math.min();//returns highest value within the list
+val = Math.max();//returns lowest value within the list
+val = Math.random();
+
+val = Math.floor(Math.random() * 20 + 1);
+
+console.log(val);
